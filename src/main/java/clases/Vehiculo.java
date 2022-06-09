e2265a754a5c445e7a079d98a92b6dbcb0ad219c
NightmareWolf95 <62491381+NightmareWolf95@users.noreply.github.com>
2022-06-09 13:03:28 -0400

    Vehiculo, Moto

--- src/main/java/clases/Vehiculo.java
+++ src/main/java/clases/Vehiculo.java
@@ -1,5 +1,36 @@
 package clases;
-
 public class Vehiculo {
-    
+    protected String nombre;
+    protected String modelo;
+    protected int anioFabricacion;
+
+    public Vehiculo(String nombre, String modelo, int anioFabricacion) {
+        this.nombre = nombre;
+        this.modelo = modelo;
+        this.anioFabricacion = anioFabricacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnioFabricacion() {
+        return anioFabricacion;
+    }
+
+    public void setAnioFabricacion(int anioFabricacion) {
+        this.anioFabricacion = anioFabricacion;
+    }
 }
