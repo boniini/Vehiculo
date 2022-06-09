c31227207394b098132932645dfee869a67be747
Miguel <villarroelmiguel662@gmail.com>
2022-06-09 13:09:21 -0400

    acciones y avion

--- src/main/java/clases/Avion.java
+++ src/main/java/clases/Avion.java
@@ -1,5 +1,17 @@
 package clases;
-
 public class Avion {
+    private int Ocupantes;
+
+    public Avion(int Ocupantes) {
+        this.Ocupantes = Ocupantes;
+    }
+
+    public int getOcupantes() {
+        return Ocupantes;
+    }
+
+    public void setOcupantes(int Ocupantes) {
+        this.Ocupantes = Ocupantes;
+    }
     
 }
